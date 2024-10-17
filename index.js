0c72be3141e812cf25a6ad393b81ad82ac0e7a76
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-10-17 15:44:20 +0200

    axios communicates with the open-meteo

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { forecast: "Hello" });
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+    );
+    res.render("index.ejs", { forecast: JSON.stringify(result.data) });
+  } catch {}
 });
 
 app.listen(port, () => {
