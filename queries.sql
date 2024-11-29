c5dac4ffc98ca150378fecc3de600807099356ad
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-11-29 12:14:22 +0100

    query for inserting basic hills records

--- queries.sql
+++ queries.sql
@@ -7,3 +7,11 @@ CREATE TABLE hills (
 	latitude DECIMAL(10,6) NOT NULL,
 	longitude DECIMAL(10,6) NOT NULL
 )
+
+-- Inserting starting records --
+INSERT INTO hills (name, height, mountain, latitude, longitude) VALUES ('Sněžka', 1603, 'Krkonoše', 50.736111, 15.739722),
+('Praděd', 1491, 'Hrubý Jeseník', 50.083056, 17.230833),
+('Lysá hora', 1324, 'Moravskoslezské Beskydy', 49.546111, 18.447222), 
+('Klínovec', 1244, 'Krušné hory', 50.396389, 12.967778), 
+('Kralický Sněžník', 1423, 'Kralický Sněžník', 50.2075, 16.8475);
+
